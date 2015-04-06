a23d27f9fe25c3aa1824028d45e776ba21f83f2f
livefire2015 <zyangye@gmail.com>
2015-04-06 14:47:38 -0700

    Use datetime for date
    Please check whether you can run this updated readFile.py on your Mac.

--- PubToNews/src/readFile.py
+++ PubToNews/src/readFile.py
@@ -1,9 +1,17 @@
+'''
+ArticleReader API:
+
+readFile - Given a spinn3r file, read in a list of article dictionary. Each article dictionary has at most five keys: url, date, title, content, and quotes, where the value of date is a datetime object and the value of quotes is a list.
+getArticleList - Get the list of articles
+sortArticleByDate - Sort the articles by date
+'''
+
+import datetime as dt 
+
+NEWS_TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
+
 class ArticleReader(object):
-    '''
-       Given a spinn3r file, read in a list of article dictionary. 
-       Each article dictionary has five keys: url, date, title, content, and quotes, where the value of quotes is a list.
-       
-    '''
+
     def __init__(self):
         self.articleList = []
         
@@ -22,7 +30,7 @@ class ArticleReader(object):
             if key == 'U':
                 self.articleList[-1]['url'] = value
             if key == 'D':
-                self.articleList[-1]['date'] = value
+                self.articleList[-1]['date'] = dt.datetime.strptime(value.strip(), NEWS_TIMEFORMAT)
             if key == 'T':
                 self.articleList[-1]['title'] = value
             if key == 'C':
@@ -37,16 +45,23 @@ class ArticleReader(object):
                     self.articleList[-1]['quotes'] = quoteList
     def getArticleList(self):
         return self.articleList
+    
+    def sortArticleByDate(self):
+        self.articleList.sort(key=lambda a : a.get('date'))
+                            
         
         
-if __name__ == '__main__':
-    ar = ArticleReader() 
-    ar.readFile('sample_of_sample.txt')     
-    for a in ar.getArticleList():
-        print "--------"
-        for k,v in a.items():
-            if k != 'quotes':
-                print k + " : " + v    
-            else:
-                for q in v:
-                    print k + " : " + q                 
+# if __name__ == '__main__':
+#     ar = ArticleReader() 
+#     ar.readFile('sample_of_sample2.txt')     
+#     ar.sortArticleByDate()
+#     for a in ar.getArticleList():
+#         print "--------"
+#         for k,v in a.items():
+#             if k != 'quotes' and k != 'date':
+#                 print k + " : " + v    
+#             if k == 'date':
+#                 print k + " : " + v.strftime(NEWS_TIMEFORMAT)
+#             if k == 'quotes':
+#                 for q in v:
+#                     print k + " : " + q                 
