3d0eb8adcd5e0c56c608d4a40907ba204fa15ee5
Cong <MC@amaris-air.stanford.edu>
2015-04-07 03:31:10 -0700

    fixed bugs in running runner.py

--- PubToNews/src/runner.py
+++ PubToNews/src/runner.py
@@ -10,13 +10,12 @@ from selection import select_by_url
 import FormatArticles as fa
 
 ar = ArticleReader()
-ar.readFile('sample_of_sample.txt')
 
-print 'readfing files'
+print "reading files"
+ar.readFile('part-r-00011')
 
 sr = SelectedArticle()
 key = set(['obama', 'gore'])
-url = set(['google.com', 'acs.org'])
 
 print "select by url"
 sr = select_by_url(ar, sr)
@@ -27,23 +26,31 @@ print "################"
 
 for a in sr.getArticleList():
         print "--------"
-        print a['title']
+        cur_t=a.get('title', None)
+        if cur_t is not None:
+                print cur_t
 
+print "##################"
+print "select by keywords"
+sr = select_by_keywords(sr, SelectedArticle(), key, 'all')
+tol = sr.count
+for a in sr.getArticleList():
+        print "--------"
+        cur_t=a.get('title', None)
+        if cur_t is not None:
+                print cur_t
 
-for article in ar.getArticleList():
+print "#################"
+print "formatting"
+
+for article in sr.getArticleList():
     cur_t=article.get('title', None)
     cur_c=article.get('content', None)
     if cur_t is not None:
         article['title']=fa.convert_to_match_array(cur_t)
     if cur_c is not None:
         article['content']=fa.convert_to_match_array(cur_c)
-    
-print "select by keywords"
-sr = select_by_keywords(sr, SelectedArticle(), key, 'all')
-tol = sr.count
-for a in sr.getArticleList():
-        print "--------"
-        print a['title']
+
 
 
 print tol
