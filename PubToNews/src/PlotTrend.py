373f7871501dcf88312b31bffa78f8384f3f8f1b
olivegwq <olivegwq@gmail.com>
2015-04-08 23:54:49 -0700

    not working

--- PubToNews/src/PlotTrend.py
+++ PubToNews/src/PlotTrend.py
@@ -10,30 +10,37 @@ import itertools
 
 DATE_TIMEFORMAT = "%Y-%m-%d"
 
-def PlotTrend(filename, timeunit):
+def PlotTrend(filename, timeuit):
     f=open(filename)
-    list_of_dates=[]
     
-    if timeunit is 'week'
+    if timeuit is 'w':
        ind = 1
-       else if timeunit is 'month'
-            ind = 0
-            else:
-                print 'Wrong time unit. Please choose week or month'
-                return
-    for line in f:
+    elif timeuit is 'month':
+           ind = 0
+    else:
+        print 'Wrong time unit. Please choose week or month'
+        return
+   
+        
+    if ind == 1:
+        list_of_weeks=[]
+        for line in f:
             fields = line.split()
-            if ind == 1:
-                list_of_weeks=[]
-                week_count=(dt.datetime.strptime(fields[1],DATE_TIMEFORMAT).date()).isocalendar()[1]
-                week_idx=fields[1](:4)+str(week_count)
-                list_of_weeks.append(week_idx)
-                grouped_week = [[d, len(list(g))] for d,g in itertools.groupby(list_of_weeks, lambda k: k)]
-                print(grouped_week)
-            else:
-                list_of_months=[]
-                month_idx=fields[1](:6)
-                list_of_months.append(month_idx)
-                grouped_month = [[d, len(list(g))] for d,g in itertools.groupby(list_of_months, lambda k: k)]
-                print(grouped_month)
+            week_count=(dt.datetime.strptime(fields[1],DATE_TIMEFORMAT).date()).isocalendar()[1]
+            week_idx=fields[1][:5]+str(week_count)
+            list_of_weeks.append(week_idx)
+        f.close()
+        grouped_week = [[d, len(list(g))] for d,g in itertools.groupby(list_of_weeks, lambda k: k)]
+        print(grouped_week)
+        #return grouped_week
+    elif ind ==0:
+        list_of_months=[]        
+        for line in f:
+            fields = line.split()
+            month_idx=fields[1][:7]
+            list_of_months.append(month_idx)
+        f.close()
+        grouped_month = [[d, len(list(g))] for d,g in itertools.groupby(list_of_months, lambda k: k)]
+        print(grouped_month)
+        #return grouped_month
    
