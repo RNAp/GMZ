77c47f4e66efc8b6aa04186a8319cd5ef332eb3b
Cong <MC@amaris-air.stanford.edu>
2015-04-10 03:02:49 -0700

    first version of similarity.py. Not tested yet. Absolute no guarrenttee of working.

---
+++ PubToNews/src/similarity.py
@@ -0,0 +1,46 @@
+from readFile import ArticleReader
+from selectedArticle import SelectedArticle
+import selection
+
+
+'''
+The following method takes in a seed news and a news article and tries to calculate Jaccard similarity for content info S & T/ (S or T)
+The news articles are in dict format
+'''
+
+def _calSimScore(seed, ar):
+    seed_c = set(seed['content'])
+    ar_c = set(ar['content'])
+
+    combSet = seed_c+ar_c
+    overlapSet = combSet- ((combSet-seed_c) + (combSet-ar_c))
+
+    num_comb = len(combSet)
+    num_overlap = len(overlapSet)
+
+    score = num_overlap/num_comb
+    
+    return score
+
+def _findSeed(seedID, AR):
+    seed = {}
+    for article in AR.getArticleList():
+        if seedID == article['ID']:
+            seed = article
+        
+    return seed
+
+'''
+The following method takes in seedID and a list of articles--ArticleReader, and finds seed news, and calculate similarity scores for each article
+'''
+def simScore(seedID, ArticleReader):
+    scoreDict = {} # 'ID' -> Score
+    seed = _findSeed(seedID, ArticleReader)
+    cur_score = 0.0
+    
+    for article in ArticleReader.getArticleList():
+        if seedID != article['ID'] and scoreDict.get(article['ID'], None) is None:
+            cur_score = calSimScore(seed, article)
+            scoreDict.append({article['ID'], cur_score})
+    
+    return scoreList
