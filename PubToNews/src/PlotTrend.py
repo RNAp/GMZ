a05a7b079380cea0fbc48ad942fbf8cc4d5bc8f0
olivegwq <olivegwq@gmail.com>
2015-04-09 22:16:21 -0700

    finally working

--- PubToNews/src/PlotTrend.py
+++ PubToNews/src/PlotTrend.py
@@ -5,15 +5,14 @@ PlotTrend: Plot the volumen fo publication in time unit (day/week/month) as a fu
 import datetime as dt 
 import string
 import itertools
-#from pyplot import hist
-#from matplotlib.dates import date2num
+import matplotlib.pyplot as plt
 
 DATE_TIMEFORMAT = "%Y-%m-%d"
 
 def PlotTrend(filename, timeuit):
     f=open(filename)
     
-    if timeuit is 'w':
+    if timeuit is 'week':
        ind = 1
     elif timeuit is 'month':
            ind = 0
@@ -27,20 +26,29 @@ def PlotTrend(filename, timeuit):
         for line in f:
             fields = line.split()
             week_count=(dt.datetime.strptime(fields[1],DATE_TIMEFORMAT).date()).isocalendar()[1]
-            week_idx=fields[1][:5]+str(week_count)
+            week_idx=fields[1][:4]+str(week_count)
             list_of_weeks.append(week_idx)
         f.close()
         grouped_week = [[d, len(list(g))] for d,g in itertools.groupby(list_of_weeks, lambda k: k)]
-        print(grouped_week)
+        count=[x[1] for x in grouped_week]
+        week_lb=[x[0] for x in grouped_week]
+        plt.plot(range(len(count)),count)
+        plt.xticks(range(len(count)),week_lb)
+        plt.show()
         #return grouped_week
     elif ind ==0:
         list_of_months=[]        
         for line in f:
             fields = line.split()
-            month_idx=fields[1][:7]
+            month_idx=fields[1][:4]+fields[1][5:]
             list_of_months.append(month_idx)
         f.close()
         grouped_month = [[d, len(list(g))] for d,g in itertools.groupby(list_of_months, lambda k: k)]
-        print(grouped_month)
+        count=[x[1] for x in grouped_month]
+        month_lb=[x[0] for x in grouped_month]
+        plt.plot(range(len(count)),count)
+        plt.xticks(rotation=90)
+        plt.xticks(range(len(count)),month_lb)
+        plt.show()
         #return grouped_month
    
