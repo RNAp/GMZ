4ddb5365b83d53d65452e7e74cee38af32d42169
olivegwq <olivegwq@gmail.com>
2015-04-10 20:48:13 -0700

    divide reading hist info func and plot func

--- PubToNews/src/PlotUtilities.py
+++ PubToNews/src/PlotUtilities.py
@@ -9,45 +9,64 @@ import matplotlib.pyplot as plt
 
 DATE_TIMEFORMAT = "%Y-%m-%d"
 
-def PlotTrend(filename, timeuit):
-    f=open(filename)
-    
+def ExtracHist(input_filename, timeuit, output_filename):
+    #Decide the time unit        
     if timeuit is 'week':
-       ind = 1
+        ind = 1
     elif timeuit is 'month':
-           ind = 0
+            ind = 0
     else:
         print 'Wrong time unit. Please choose week or month'
         return
    
+    with open(input_filename, 'r') as f:       
+        if ind == 1:#Take week as time unit
+           list_of_weeks=[]
+           for line in f:
+               #read in the info for an article
+               fields = line.split()
+               #Decide the which week this day is in that year
+               week_count=(dt.datetime.strptime(fields[1],DATE_TIMEFORMAT).date()).isocalendar()[1]
+               #Give an unique lable for that week and assigned it to this article
+               week_idx=fields[1][:4]+str(week_count)
+               list_of_weeks.append(week_idx)
+           f.close()
+           #group articles by their week label, generate a list of pairs of  [weeklable, count]
+           grouped= [[d, len(list(g))] for d,g in itertools.groupby(list_of_weeks, lambda k: k)]
         
-    if ind == 1:
-        list_of_weeks=[]
-        for line in f:
-            fields = line.split()
-            week_count=(dt.datetime.strptime(fields[1],DATE_TIMEFORMAT).date()).isocalendar()[1]
-            week_idx=fields[1][:4]+str(week_count)
-            list_of_weeks.append(week_idx)
-        f.close()
-        grouped_week = [[d, len(list(g))] for d,g in itertools.groupby(list_of_weeks, lambda k: k)]
-        count=[x[1] for x in grouped_week]
-        week_lb=[x[0] for x in grouped_week]
-        plt.plot(range(len(count)),count)
-        plt.xticks(rotation=90)
-        plt.xticks(range(len(count)),week_lb)
-        plt.show()
-    elif ind ==0:
-        list_of_months=[]        
-        for line in f:
-            fields = line.split()
-            month_idx=fields[1][:4]+fields[1][5:7]
-            list_of_months.append(month_idx)
-        f.close()
-        grouped_month = [[d, len(list(g))] for d,g in itertools.groupby(list_of_months, lambda k: k)]
-        count=[x[1] for x in grouped_month]
-        month_lb=[x[0] for x in grouped_month]
-        plt.plot(range(len(count)),count)
-        plt.xticks(rotation=90)
-        plt.xticks(range(len(count)),month_lb)
-        plt.show()
-   
+        elif ind ==0:
+            list_of_months=[]        
+            for line in f:
+                #read in the info for an article
+                fields = line.split()
+                #Give an unique lable for that month and assigned it to this article
+                month_idx=fields[1][:4]+fields[1][5:7]
+                list_of_months.append(month_idx)
+            f.close()
+            #group articles by their week label, generate a list of pairs of  [monthlable, count]
+            grouped= [[d, len(list(g))] for d,g in itertools.groupby(list_of_months, lambda k: k)]
+            #print grouped
+    f.close()
+    #Start file writing
+    with open(output_filename,'w') as f:
+        for cp in grouped:
+            f.write(cp[0])
+            f.write("\t %s " % str(cp[1]))
+            f.write("\n")
+    f.close
+    
+    
+def PlotHist(input_filename):
+    with open(input_filename, 'r') as f: 
+         label=[]
+         count=[]
+         for line in f:
+             fields=line.split()
+             label.append(fields[0])
+             count.append(fields[1])
+         plt.plot(range(len(count)),count)
+         plt.xticks(rotation=90)
+         plt.xticks(range(len(count)),label)
+         plt.show()
+    return
+             
