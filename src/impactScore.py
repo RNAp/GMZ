116dd8b5ed5a0fa4d90008799bff89f974e9901f
livefire2015 <zyangye@gmail.com>
2015-05-25 22:20:30 -0700

    Update impactScore.py

--- src/impactScore.py
+++ src/impactScore.py
@@ -1,14 +1,17 @@
 '''
-The purpose of this code is to calculate a normalized impact score for a publication based on weighted-average of visiting traffic of its reporting media souces. 
-
-
+The purpose of this code is to calculate a normalized impact score for a publication based on weighted-average of visiting traffic of its reporting media souces.
 '''
 from urlparse import urlparse
 
 def findDomain(url): # a method that takes in a url string and return the domain of it
+    # print url
     parsed=urlparse(url.strip())
     originalDomain = '{uri.scheme}://{uri.netloc}'.format(uri=parsed)
     fields = originalDomain.split('//')
+
+    if len(fields[1].split('.')) == 4:
+        ffields = fields[1].split('.', 2)
+        domain = ffields[2]
     if len(fields[1].split('.')) == 3:
         ffields = fields[1].split('.', 1)
         domain = ffields[1]
@@ -20,6 +23,7 @@ def findDomain(url): # a method that takes in a url string and return the domain
 def _calDomainTraffic(domainDict, domain):
     for d, t in domainDict.items():
         if d.strip() == domain.strip():
+            print d
             return t
     return 0
 
@@ -38,12 +42,12 @@ def _readLineAndSplit(f):
         for line in f:
             yield line.split('\t')
     
-def impactScore(relevantNewsFilename, urlRankFile="url_rank.txt"):
+def impactScore(relevantNewsFilename, seperator, urlRankFile="url_rank.txt"):
     domainDict = _readUrlRank(urlRankFile)
     score = 0.0
     with open(relevantNewsFilename) as f:
-        data = _readLineAndSplit(f)
-        for fields in data:
+        for line in f:
+            fields = line.split(seperator)
             url = fields[2].strip()
             domain = findDomain(url)
             score += _calDomainTraffic(domainDict, domain)
@@ -51,9 +55,4 @@ def impactScore(relevantNewsFilename, urlRankFile="url_rank.txt"):
 
 
 if __name__ == "__main__":
-    print impactScore("2015Q1_PlantDev3KeyWords_2015010220_00046943_W_relevant.txt") # result: 0.00514308239043
-    
-    
-    
-
-
+    print impactScore("qdots_2014032401_00039532_W.csv",',')
